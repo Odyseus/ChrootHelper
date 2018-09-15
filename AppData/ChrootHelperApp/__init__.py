2eba5a5551ea58242445bdcdfa156e4bf8e4bd2c
Odyseus <venusyadonis@gmail.com>
2018-09-15 01:24:29 -0300

    Bump version.

--- AppData/ChrootHelperApp/__init__.py
+++ AppData/ChrootHelperApp/__init__.py
@@ -4,4 +4,4 @@
 __status__ = "(Unstable)"
 __appname__ = "Chroot Helper"
 __appdescription__ = "Helper utility to assist in the basic setup of a chroot environment to be able to perform rescue operations."
-__version__ = "2018-07-03 05:17:37.442"
+__version__ = "2018-09-15 01:19:13.333"
