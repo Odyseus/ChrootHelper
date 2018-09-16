8085eac6ef60fa198e119b5adbd6f552c21a90e8
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:38:42 -0300

    Added README and updated __init__.py module.

--- AppData/ChrootHelperApp/__init__.py
+++ AppData/ChrootHelperApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Unstable)"
 __appname__ = "Chroot Helper"
 __appdescription__ = "Helper utility to assist in the basic setup of a chroot environment to be able to perform rescue operations."
 __version__ = "2018-09-15 01:19:13.333"
+
+
+if __name__ == "__main__":
+    pass
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Chroot Helper ([documentation](https://pythoncliapplications.gitlab.io/CLIApplicationsManager/includes/ChrootHelper/index.html))
+
+Helper utility to assist in the basic setup of a chroot environment to be able to perform rescue operations.
