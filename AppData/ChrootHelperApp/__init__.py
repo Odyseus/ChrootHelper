0d3b8d062d370505c7f2f8f28770be1c3405939b
Odyseus <venusyadonis@gmail.com>
2018-10-06 10:30:16 -0300

    Python modules
    - Updated all modules to an standardized organization of import statements.
    - Cleaned up dosctrings.
    - `__init__.py` module:
        - Removed unnecessary parentheses from the __status__ property.
    - `cli.py` module:
        - Redesigned to use the python_utils.cli_utils module.
        - Standardized organization of the usage string.

--- AppData/ChrootHelperApp/__init__.py
+++ AppData/ChrootHelperApp/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-__status__ = "(Unstable)"
+__status__ = "Unstable"
 __appname__ = "Chroot Helper"
 __appdescription__ = "Helper utility to assist in the basic setup of a chroot environment to be able to perform rescue operations."
 __version__ = "2018-09-15 01:19:13.333"
