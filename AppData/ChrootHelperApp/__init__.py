b8cd6b272a87ceff1f7534421419b7db85fff790
Odyseus <venusyadonis@gmail.com>
2021-09-27 08:42:32 -0300

    Updated manual page and bump version

--- AppData/ChrootHelperApp/__init__.py
+++ AppData/ChrootHelperApp/__init__.py
@@ -3,7 +3,7 @@
 __status__ = "Unstable"
 __appname__ = "Chroot Helper"
 __appdescription__ = "CLI utility to assist in the basic setup of a chroot environment to be able to perform rescue operations."
-__version__ = "2020-03-20 21:47:57.127"
+__version__ = "2021-09-27 08:34:09.279"
 
 
 if __name__ == "__main__":
